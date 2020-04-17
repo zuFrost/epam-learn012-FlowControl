aef09db07ea769e066322bf7565d38543dd5349e
Alex Frost <zufrost.a@gmail.com>
2020-04-17 13:42:43 +0300

    create class LearnMainArrayResizing

---
+++ src/com/zufrost/learn/array/LearnMainArrayResizing.java
@@ -0,0 +1,8 @@
+package com.zufrost.learn.array;
+
+public class LearnMainArrayResizing {
+    public static void main(String[] args) {
+        int [] array = new int[7];
+        array = new int[21];
+    }
+}
