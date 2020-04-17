1ac5d460dec11a356f41f09ac565643e35909672
Alex Frost <zufrost.a@gmail.com>
2020-04-17 10:32:29 +0300

    create class LearnMainArray

---
+++ src/com/zufrost/learn/array/LearnMainArray.java
@@ -0,0 +1,9 @@
+package com.zufrost.learn.array;
+
+public class LearnMainArray {
+    public static void main(String[] args) {
+        int arr1 [] = {2, 3, 5};
+        int arr2 [] = new int[-3];
+
+    }
+}
