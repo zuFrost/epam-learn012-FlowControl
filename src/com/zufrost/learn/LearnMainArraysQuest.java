93bb11f87800ea09a01190ba28468490dd0c9940
Alex Frost <zufrost.a@gmail.com>
2020-04-17 10:08:18 +0300

    create class LearnMainArraysQuest

---
+++ src/com/zufrost/learn/LearnMainArraysQuest.java
@@ -0,0 +1,19 @@
+package com.zufrost.learn;
+
+public class LearnMainArraysQuest {
+    public static void main(String[] args) {
+
+        int[] mas1 = new int [24];
+        Integer mas2 [] = new Integer [24];
+//        char[] mas3 = new Character [] {'a', 'b', 'c'};
+//        Item [] mas4 = new Item {new Item (), new Item ()};
+        double[] mas5 = {5, 10, 15, 20};
+        int[] mas6 [] = new int [4] [5];
+        int mas7 [] [] = new int [4] [];
+
+
+
+
+
+    }
+}
